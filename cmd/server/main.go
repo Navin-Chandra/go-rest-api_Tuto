36647666a05c3ba67f639012bfaa49b24cfce5a8
navin <navin@example.com>
2021-04-21 00:29:45 +0530

    Added first end point

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/Navin-Chandra/go-rest-api_Tuto/internal/transport/http"
+)
+
+// App - the struct which contains things like
+// pointers to database connections
+type App struct{}
+
+// Run - handles the startup of our application
+func (app *App) Run() error {
+	fmt.Println("Setting Up Our App")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("Go REST API Course")
+	// fmt.Println("Go REST API Course")\
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error Starting Up")
+		fmt.Println(err)
+	}
 }
